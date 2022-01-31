c9bd664db25fcb07985213a101042ba362018d71
705H-S <93358442+705h-S@users.noreply.github.com>
2022-01-30 21:28:59 -0800

    Added a server.js with express and ports

---
+++ server.js
@@ -0,0 +1,17 @@
+// require 
+const express = require ("express");
+const path = require ("path");
+
+const app = express();
+// port
+const PORT = process.env.PORT || 3002;
+
+// app express ()
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+// listener
+app.listen(PORT, () =>
+    console.log(`Ready on port ${PORT}`)
+);
